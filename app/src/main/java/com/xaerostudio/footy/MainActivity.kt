7ef51b3b27ecba637edc742e6d1cf9397e530611
xaero yuan <xaeroyuan@gmail.com>
2023-09-30 23:07:56 +1000

    Initial commit

---
+++ app/src/main/java/com/xaerostudio/footy/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.xaerostudio.footy
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.xaerostudio.footy.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navView = binding.bottomNavigationView2
+        val navController: NavController = findNavController(R.id.nav_host_fragment)
+        val appBarConfiguration: AppBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.recipesFragment,
+                R.id.favoriteFragment,
+                R.id.foodJokeFragment
+            )
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
+
+    }
+}
