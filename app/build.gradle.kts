01ea3f6e15f8d780a943b7a5b34779c148d210f5
xaero yuan <xaeroyuan@gmail.com>
2023-10-01 22:27:54 +1100

    recipes row layout view

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,7 +40,7 @@ android {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.9.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
@@ -49,4 +49,7 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    implementation("com.facebook.shimmer:shimmer:0.5.0")
+    implementation("com.todkars:shimmer-recyclerview:0.4.1")
+    implementation("io.coil-kt:coil:0.13.0")
 }
