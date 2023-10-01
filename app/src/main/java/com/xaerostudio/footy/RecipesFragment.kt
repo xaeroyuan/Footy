69c408b4a262ba6c4f0042a3e83f16d16a67f5ca
xaero yuan <xaeroyuan@gmail.com>
2023-10-01 23:04:03 +1100

    setup recycler view

--- app/src/main/java/com/xaerostudio/footy/RecipesFragment.kt
+++ app/src/main/java/com/xaerostudio/footy/RecipesFragment.kt
@@ -5,15 +5,25 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.xaerostudio.footy.databinding.FragmentRecipesBinding
 
 class RecipesFragment : Fragment() {
-
+    private var _binding: FragmentRecipesBinding? = null
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_recipes, container, false)
+        _binding = FragmentRecipesBinding.inflate(inflater, container, false)
+        val view = binding.root
+        binding.recycleview.showShimmer()
+        return view
     }
-
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
